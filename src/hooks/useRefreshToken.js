cd3b6b36fbdec09e8879f57bc41f378fff8b8b74
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2023-11-13 16:14:47 +0100

    add code

---
+++ src/hooks/useRefreshToken.js
@@ -0,0 +1,25 @@
+import axios from '../api/axios';
+import useAuth from './useAuth';
+
+const useRefreshToken = () => {
+    const { setAuth } = useAuth();
+
+    const refresh = async () => {
+        const response = await axios.post('/auth/refresh-token', {}, {
+            withCredentials: true
+        });
+        setAuth(prev => {
+            console.log(JSON.stringify(prev));
+            console.log(response.data.access_token);
+            return {
+                ...prev,
+                roles: response.data.roles,
+                accessToken: response.data.access_token
+            }
+        });
+        return response.data.access_token;
+    }
+    return refresh;
+};
+
+export default useRefreshToken;
