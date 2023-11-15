8318ea2f13289de48f6d30a43086a009375e92a7
Remi Jorge <remijorge5@gmail.com>
2023-11-15 09:59:21 +0100

    design test with  material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Register from './components/Register';
-import Login from './components/Login';
+import Login from './views/Login';
 import Home from './components/Home';
 import Layout from './components/Layout';
 import Editor from './components/Editor';
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from 'react';
 import useAuth from '../hooks/useAuth';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import {TextField, Button, Stack, Container} from '@mui/material';
 
 import axios from '../api/axios';
 const LOGIN_URL = '/auth/token';
@@ -78,45 +79,51 @@ const Login = () => {
     return (
 
         <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input
-                    type="text"
-                    id="username2"
-                    ref={userRef}
-                    autoComplete="off"
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                    required
-                />
+            
+                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                <h1>Sign In</h1>
 
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                    required
-                />
-                <button>Sign In</button>
-                <div className="persistCheck">
-                    <input
-                        type="checkbox"
-                        id="persist"
-                        onChange={togglePersist}
-                        checked={persist}
-                    />
-                    <label htmlFor="persist">Trust This Device</label>
-                </div>
-            </form>
-            <p>
-                Need an Account?<br />
-                <span className="line">
-                    <Link to="/register">Sign Up</Link>
-                </span>
-            </p>
+                <form onSubmit={handleSubmit}>
+                    <Stack spacing={2} direction="column" sx={{ width: '100%' }}>
+                        <TextField
+                            id="username"
+                            label="Username"
+                            variant="outlined"
+                            inputRef={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                            />
+
+                        <TextField
+                            id="password"
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                            />
+                        <Button variant="contained" type="submit">Sign In</Button>
+                        <div className="persistCheck">
+                            <input
+                                type="checkbox"
+                                id="persist"
+                                onChange={togglePersist}
+                                checked={persist}
+                                />
+                            <label htmlFor="persist">Trust This Device</label>
+                        </div>
+                    </Stack>
+                </form>
+                <p>
+                    Need an Account?<br />
+                    <span className="line">
+                        <Link to="/register">Sign Up</Link>
+                    </span>
+                </p>
+
         </section>
 
     )
