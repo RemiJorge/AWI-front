febabdb332ee2f9a47699dbce8c23919948d8e5a
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2024-02-04 02:02:27 +0100

    update imports for messages

--- src/views/Messages.js
+++ src/views/Messages.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
-import MessageUser from './MessageUser';
+import MessageUser from '../components/MessageUser';
 
 const Messages = () => {
     const axiosPrivate = useAxiosPrivate();
